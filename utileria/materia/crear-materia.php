7c1cc3f2caf7a1ada4b71cd2805ff8330557cc00
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-03-24 23:37:06 -0600

    Modifircar nombremateria y nombre clase estaban invertidos

--- utileria/materia/crear-materia.php
+++ utileria/materia/crear-materia.php
@@ -6,7 +6,7 @@ try {
   $cadena_base .= '0123456789';
   $limite = strlen($cadena_base) - 1;
   $claveAccesoClase = '';
-  for($i=0; $i < 10; $i++) { 
+  for($i=0; $i < 10; $i++) {
     $claveAccesoClase .= $cadena_base[rand(0, $limite)];
   }
   $nombreClase = htmlentities(addslashes($_POST['nombreClase']));
@@ -16,14 +16,7 @@ try {
   $aulaClase = htmlentities(addslashes($_POST['aulaClase']));
   $anio = htmlentities(addslashes($_POST['anoClase']));
   $anoClase = substr($anio, 0, 4);
-  $cicloEscolar = htmlentities(addslashes($_POST['cicloEscolarClase']));
-  if($cicloEscolar == 'cicloA') {
-    $cicloEscolarClase = 'A';
-  } else if($cicloEscolar == 'cicloB') {
-    $cicloEscolarClase = 'B';
-  } else if($cicloEscolar == 'cicloV') {
-    $cicloEscolarClase = 'V';
-  }
+  $cicloEscolarClase = htmlentities(addslashes($_POST['cicloEscolarClase']));
   $codigoProfesorClase = htmlentities(addslashes($_POST['codigoProfesorClase']));
 
   $sql = 'SELECT * FROM clase WHERE nrc = :nrc';
@@ -32,9 +25,9 @@ try {
   $resultado->execute();
   $numRow = $resultado->rowCount();
   if($numRow == 0) {
-    $sql = 'INSERT INTO clase (claveAcceso, nombreMateria, nrc, claveSeccion, nombreClase, aula, anio, cicloEscolar, ProfesorUsuario_codigoProfesor) VALUES (:ca, :nm, :n, :cs, :nc, :a, :y, :ce, :pucp)';
+    $sql = 'INSERT INTO clase (claveAcceso, nombreMateria, nrc, claveSeccion, nombreClase, aula, anio, CicloEscolar_idCicloEscolar, ProfesorUsuario_codigoProfesor) VALUES (:ca, :nm, :n, :cs, :nc, :a, :y, :ce, :pucp)';
     $resultado = $baseDatos->prepare($sql);
-    $array = array(':ca'=>$claveAccesoClase, ':nm'=>$nombreClase, ':n'=>$nrcClase, ':cs'=>$seccionClase, ':nc'=>$materiaClase, ':a'=>$aulaClase, ':y'=>$anoClase, ':ce'=>$cicloEscolarClase, ':pucp'=>$codigoProfesorClase);
+    $array = array(':ca'=>$claveAccesoClase, ':nm'=>$materiaClase, ':n'=>$nrcClase, ':cs'=>$seccionClase, ':nc'=>$nombreClase ':a'=>$aulaClase, ':y'=>$anoClase, ':ce'=>$cicloEscolarClase, ':pucp'=>$codigoProfesorClase);
     $resultado->execute($array);
     echo 'success';
   } else {
