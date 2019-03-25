5a6496c9415081751b2c5fcc374e3fe71169dc85
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-03-24 23:39:27 -0600

    Nombremateria y nombreclase se invertian al actualizar los datos

--- utileria/materia/editar-materia.php
+++ utileria/materia/editar-materia.php
@@ -9,19 +9,12 @@ try {
     $aulaClase = $_POST['aulaClase'];
     $anio = $_POST['anoClase'];
     $anoClase = substr($anio, 0, 4);
-    $cicloEscolar = $_POST['cicloEscolarClase'];
-    if($cicloEscolar == 'cicloA') {
-        $cicloEscolarClase = 'A';
-    } else if($cicloEscolar == 'cicloB') {
-        $cicloEscolarClase = 'B';
-    } else if($cicloEscolar == 'cicloV') {
-        $cicloEscolarClase = 'V';
-    }
+    $cicloEscolarClase = $_POST['cicloEscolarClase'];
     $codigoProfesorClase = $_POST['codigoProfesorClase'];
 
-    $sql = "UPDATE clase SET nombreMateria = :nm, nrc = :n, claveSeccion = :cs, nombreClase = :nc, aula = :a, anio = :y, cicloEscolar = :ce WHERE claveAcceso = :ca AND ProfesorUsuario_codigoProfesor = :pucp";
+    $sql = "UPDATE clase SET nombreMateria = :nm, nrc = :n, claveSeccion = :cs, nombreClase = :nc, aula = :a, anio = :y, CicloEscolar_idCicloEscolar = :ce WHERE claveAcceso = :ca AND ProfesorUsuario_codigoProfesor = :pucp";
     $resultado = $baseDatos->prepare($sql);
-    $array = array(':nm'=>$nombreClase, ':n'=>$nrcClase, ':cs'=>$seccionClase, ':nc'=>$materiaClase, ':a'=>$aulaClase, ':y'=>$anoClase, ':ce'=>$cicloEscolarClase, ':ca'=>$claveAccesoClase, ':pucp'=>$codigoProfesorClase);
+    $array = array(':nm'=>$materiaClase, ':n'=>$nrcClase, ':cs'=>$seccionClase, ':nc'=>$nombreClase, ':a'=>$aulaClase, ':y'=>$anoClase, ':ce'=>$cicloEscolarClase, ':ca'=>$claveAccesoClase, ':pucp'=>$codigoProfesorClase);
     $resultado->execute($array);
     echo 'success';
 } catch(Exception $exec) {
