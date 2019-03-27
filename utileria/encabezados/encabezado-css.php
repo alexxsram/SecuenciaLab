c4846611c5a270ed2142bb96a83a5c68a037bc60
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-03-26 19:27:45 -0600

    Nada en particular

--- utileria/encabezados/encabezado-css.php
+++ utileria/encabezados/encabezado-css.php
@@ -1,5 +1,5 @@
 <head>
     <!-- Estilos (CSS) -->
     <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="fontawesome-free-5.7.2/css/all.css">  
+    <link rel="stylesheet" type="text/css" media="screen" href="fontawesome-free-5.7.2/css/all.css">
 </head>
