649b1e6d537e702219071b174a35c0ce8ace6919
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-03-26 19:25:55 -0600

    Logica para eliminar una practica

---
+++ utileria/practica/eliminar-practica.php
@@ -0,0 +1,26 @@
+<?php
+include('../operaciones/conexion.php');
+try {
+    $idPractica = htmlentities(addslashes($_POST['idPractica']));
+    $sql = 'SELECT * FROM practica WHERE idPractica = :idPractica';
+    $resultado = $baseDatos->prepare($sql);
+    $resultado->bindValue(':idPractica', $idPractica);
+    $resultado->execute();
+    if($resultado->execute()){
+      $numRow = $resultado->rowCount();
+      if($numRow == 1){
+        $sql = 'DELETE FROM practica WHERE idPractica = :idPractica';
+        $resultado = $baseDatos->prepare($sql);
+        $resultado->bindValue(':idPractica', $idPractica);
+        $resultado->execute();
+        echo "success";
+      }else{
+        echo "Error. No existe el ID de la práctica.";
+      }
+    }else{
+      echo "Error. No se pudo comprobar la práctica que se desea eliminar.";
+    }
+} catch(Exception $exec) {
+    die('Error en la base de datos: ' . $exec->getMessage());
+}
+?>
