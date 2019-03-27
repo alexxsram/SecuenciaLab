0407b70ce8ce6856499ab087b8b9ffe34909a328
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-03-26 19:27:34 -0600

    Prueba para cambiar icono de las pestañas del navegador.

--- utileria/materia/eliminar-materia.php
+++ utileria/materia/eliminar-materia.php
@@ -1,12 +1,30 @@
 <?php
 include('../operaciones/conexion.php');
 try {
-    $nrcClase = htmlentities(addslashes($_POST['nrcClase']));
+    $claveAcceso = htmlentities(addslashes($_POST['claveAcceso']));
 
-    $sql = 'DELETE FROM clase WHERE nrc = :nrc';
+    $sql = 'SELECT * FROM clase WHERE claveAcceso = :claveAcceso';
     $resultado = $baseDatos->prepare($sql);
-    $resultado->bindValue(':nrc', $nrcClase);
+    $resultado->bindValue(':claveAcceso', $claveAcceso);
     $resultado->execute();
+    if($resultado->execute()){
+      $numRow = $resultado->rowCount();
+      if($numRow == 1){
+        $sql = 'DELETE FROM clase WHERE claveAcceso = :claveAcceso';
+        $resultado = $baseDatos->prepare($sql);
+        $resultado->bindValue(':claveAcceso', $claveAcceso);
+        $resultado->execute();
+        echo "success";
+      }else{
+        echo "Error. No existe la clase.";
+      }
+    }else{
+      echo "Error. No se pudo comprobar la clase que se desea eliminar.";
+    }
+
+
+
+
 } catch(Exception $exec) {
     die('Error en la base de datos: ' . $exec->getMessage());
 }
