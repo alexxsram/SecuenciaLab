11e904b9cb76b3ecc906e7992992ccf9c92f8dac
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-04-11 21:48:04 -0500

    Nada relevante

--- registrar-usuario.php
+++ registrar-usuario.php
@@ -4,10 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <?php include('utileria/encabezados/encabezado-css.php'); ?>
   <link rel="stylesheet" type="text/css" media="screen" href="css/login-container.css">
-
   <!-- Título de la página -->
   <title> SecuenciaLab - Registrar Usuario </title>
 </head>
