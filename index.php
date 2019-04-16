5d720ec91f0cc8279ac8968961b3ccdced1bebf4
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-04-16 18:42:12 -0500

    Nada

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); 
+session_start();
 if(!isset($_SESSION['codigo']) && ($_SESSION['estado'] != 'INICIO_SESION_PROFESOR' || $_SESSION['estado'] != 'INICIO_SESION_ALUMNO')) {
   header('Location: utileria/sesion/sesion.php');
 } else {
