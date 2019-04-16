71ec908097370282a6cc567e279e87b19fb341ed
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-04-15 22:10:27 -0500

    Logica para la vista de graficas de alumno

--- js/validation-form-plugin/validacion-formularios.js
+++ js/validation-form-plugin/validacion-formularios.js
@@ -1132,6 +1132,45 @@ $('#modalCrearClase').ready(function() {
   });
 });*/
 
+document.addEventListener('DOMContentLoaded', function () {
+  var myChart = Highcharts.chart('container', {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Practica #1: TRatmiento de un motor '
+    },
+    xAxis: {
+      categories: ['Categorias']
+    },
+    yAxis: {
+      title: {
+        text: 'Puntuación'
+      }
+    },
+    credits: false,
+    series: [{
+      name: 'Alumno',
+      data: [95]
+    }, {
+      name: 'Mínimo',
+      data: [60]
+    }, {
+      name: 'Máximo',
+      data: [100]
+    }, {
+      name: 'Promedio',
+      data: [87]
+    }, {
+      name: 'Media',
+      data: [77]
+    }, {
+      name: 'Moda',
+      data: [85]
+    }]
+  });
+});
+
 $(document).ready(function() {
   $.ajax({
     type: "POST",
@@ -1141,6 +1180,26 @@ $(document).ready(function() {
       //$('#listgroup22').append("<button type=\"button\" class=\"list-group-item list-group-item-action\">Estra al cargar base de datos ---</button>");
       $('#listgroup22').append(response);
       $('#listaAlumnos').append(response);
+    },
+    error: function(response) {
+      bootbox.alert("Error: " + response);
+    }
+  });
+});
+
+$('#graficas-informativas-alumno').ready(function() {
+  $.ajax({
+    type: "POST",
+    url: "utileria/materia/cargar-informacion-grafica-alumno.php",
+    type: "POST",
+    dataType: "HTML",
+    data: "claveAcceso=" + $("#info-alumno-claveAcceso").val()
+    + "&claveUsuario=" + $("#info-alumno-codigo-alumno").val(),
+    success: function(response)
+    {
+      //$('#listgroup22').append("<button type=\"button\" class=\"list-group-item list-group-item-action\">Estra al cargar base de datos ---</button>");
+      $('#listgroup22').append(response);
+      //$('#listaAlumnos').append(response);
     },
     error: function(response) {
       bootbox.alert("Error: " + response);
