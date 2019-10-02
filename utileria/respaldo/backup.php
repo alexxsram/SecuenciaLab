c16c672e253607bdda17f508fe6e96a9cfd09a2c
Alejandro Salgado <alejandrosram@outlook.com>
2019-10-02 04:25:10 -0500

    Actualizado exportación de la base de datos, archivo json con los datos de la exportación generado

--- utileria/respaldo/backup.php
+++ utileria/respaldo/backup.php
@@ -1,22 +1,22 @@
 <?php 
 $method = $_GET['method'];
 
-// $host = 'localhost';
-// $user = 'root';
-// $password = '';
-// $database = 'secuencialab';
-
 $host = 'localhost';
-$user = 'id10689217_secuencialaboperaciones';
-$password = 'develsecuencialab';
-$database = 'id10689217_secuencialab';
+$user = 'root';
+$password = '';
+$database = 'secuencialab';
+
+// $host = 'localhost';
+// $user = 'id10689217_secuencialaboperaciones';
+// $password = 'develsecuencialab';
+// $database = 'id10689217_secuencialab';
 
 switch($method) {
     case 'export':
         $rutaCarpetaBackups = realpath('../../sql/backups');
-        $fecha = date('Ymd_His');
-        $rutaDump = $rutaCarpetaBackups.'\\'.$fecha.'\\';
-        $filename = $database.'_'.$fecha.'.sql';
+        $fechaFile = date('Ymd_His');
+        $rutaDump = $rutaCarpetaBackups.'\\'.$fechaFile.'\\';
+        $filename = $database.'_'.$fechaFile.'.sql';
         if(!file_exists(is_dir($rutaDump))) {
             if(!mkdir($rutaDump, 0777, true)) {
                 $error = error_get_last();
@@ -27,6 +27,24 @@ switch($method) {
         exec($comando, $output, $resultado);
         switch($resultado) {
             case 0:
+                $archivoBackupJson = $rutaCarpetaBackups.'\\backups.json';
+                $array = array();
+                $array['dumps'] = array();
+                $array['dumps'][$fechaFile] = array('sql_filename' => $filename, 'sql_path' => $rutaDump, 'export_date' => $fechaFile);
+                if(!file_exists($archivoBackupJson)) {
+                    $json = json_encode($array, JSON_PRETTY_PRINT);
+                } else {
+                    $content = file_get_contents($archivoBackupJson);
+                    unlink($archivoBackupJson);
+                    $contentArray = json_decode($content, true);
+                    $newArray = array();
+                    $newArray['dumps'] = array_merge($contentArray['dumps'], $array['dumps']);
+                    $json = json_encode($newArray, JSON_PRETTY_PRINT);
+                }
+                $fo = fopen($archivoBackupJson, 'w');
+                fwrite($fo, $json);
+                fclose($fo);
+
                 echo 'success';
                 break;
             case 1:
