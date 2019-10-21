bcd4220e1482f84f48d803cbcdc4edc2a4dba045
Alejandro Salgado <alejandrosram@outlook.com>
2019-10-20 19:26:24 -0500

    Error en reportes de calificaciones

--- DOCS/examples/reporte-calificaciones-clase.php
+++ DOCS/examples/reporte-calificaciones-clase.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['codigo']) && ($_SESSION['estado'] != 'INICIO_SESION_PROFESOR'
-|| $_SESSION['estado'] != 'INICIO_SESION_ALUMNO')) {
+if(!isset($_SESSION['codigo']) && $_SESSION['permiso'] == '') {
   header('Location: ../sesion/sesion.php');
 }
 
