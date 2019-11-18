61ee350c49e6b260f273f65ae895679201422d26
Alejandro Salgado <alejandrosram@outlook.com>
2019-11-17 20:17:52 -0600

    Agreados metas para limpiar cache de css y js

--- utileria/encabezados/encabezado-css.php
+++ utileria/encabezados/encabezado-css.php
@@ -1,5 +1,9 @@
 <head>
   <!-- Estilos (CSS) -->
+  <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
   <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" media="screen" href="fontawesome-free-5.7.2/css/all.css">
   <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap-slider.css">
